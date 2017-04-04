e0fd9ed7b55560fbea26c47041c74bb5ec026ef6
Paweł Pigłowski <pawkonik@gmail.com>
2017-04-04 21:10:15 +0200

    refactored repositories

--- src/main/java/com/zoo/repository/DivisionRepository.java
+++ src/main/java/com/zoo/repository/DivisionRepository.java
@@ -1,7 +1,5 @@
 package com.zoo.repository;
 
-import org.springframework.transaction.annotation.Transactional;
-
 import com.zoo.domain.Division;
 
 public interface DivisionRepository extends BaseRepository<Division, Long> {
