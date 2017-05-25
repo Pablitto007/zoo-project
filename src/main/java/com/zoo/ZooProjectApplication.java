2a71f4a09971f44bd61edecb1718310472119189
Paweł Pigłowski <pawkonik@gmail.com>
2017-05-25 16:14:32 +0200

    sample data generation added

--- src/main/java/com/zoo/ZooProjectApplication.java
+++ src/main/java/com/zoo/ZooProjectApplication.java
@@ -1,7 +1,6 @@
 package com.zoo;
 
-import java.time.LocalDate;
-import java.time.Month;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -13,8 +12,8 @@ import org.springframework.context.annotation.Bean;
 import com.zoo.domain.Animal;
 import com.zoo.domain.Staff;
 import com.zoo.repository.AnimalRepository;
-import com.zoo.repository.DivisionRepository;
 import com.zoo.repository.StaffRepository;
+import com.zoo.util.SampleDataGenerator;
 
 @SpringBootApplication
 public class ZooProjectApplication 
@@ -31,20 +30,24 @@ public class ZooProjectApplication
     public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
         return args -> {
         	
-         // 1
-    		Staff Al = new Staff("Al" , "Abranow" , 'f', null, "Mammals");
-    		Animal Anatol = new Animal("Anatol" , "lion", 'M', 
-    				LocalDate.of(2013, Month.JANUARY, 1), LocalDate.of(2015, Month.JANUARY, 1));
-    		Animal Opera = new Animal("Opera" , "lion", 'F', 
-    				LocalDate.of(2008, Month.NOVEMBER, 11), null);
-    		Al.addAnimal(new Animal [] {Anatol, Opera});
-    		staffRepository.save(Al);	
-    		//2
-    		Staff Cezar = new Staff("Cezar" , "Cejrus" , 'M', null, "Reptiles");
-    		Animal turtle = new Animal("Donald" , "turtle", 'M', 
-    			LocalDate.of(2010, Month.JANUARY, 1), LocalDate.of(2010, Month.JANUARY, 1));
-    		Cezar.addAnimal(turtle);
-    		staffRepository.save(Cezar);
+        	/**
+        	 * Sample data generated via SampleDataGenerator class. 
+        	 * Each person is responsible for 2 distinct animals at the beginning.
+        	 */
+        	List<Animal> animals = SampleDataGenerator.getAnimals(40);
+        	List<Staff> staff = SampleDataGenerator.getStaff(20);
+        	
+        	for (int i = 0; i < staff.size(); i++){
+        		Staff toPersist = staff.get(i);
+        		
+        		for (int y = i*2;  y <= animals.size();){
+        		List<Animal> sublist = animals.subList(y, y+2);	
+        		Animal [] toAdd = new Animal [sublist.size()];
+        		toPersist.addAnimal(sublist.toArray(toAdd));
+        		staffRepository.save(toPersist);
+        		break;
+        		}
+        	}
         };
     }
 	public static void main(String[] args) {
